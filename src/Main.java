23a5a654cf7cc80609b77fe9efc42f797d31dc2b
Casey M. Zimmerman <zimmecas@mail.gvsu.edu>
2016-09-23 15:43:38 -0400

    test

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
 
 public class Main {
-	public static void main(String[] args){
+	public static void main(final String[] args) {
 		Model model = new Model();
 		Presenter presenter = new Presenter();
 		presenter.setModel(model);
 		View view = new View();
-		presenter.setView(view);
-		
+		presenter.setView(view);	
 	}
 }
