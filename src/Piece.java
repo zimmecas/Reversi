f5381f5fd40b19221c87545a218a6d7d30cbcf29
BrendanDent <dentb@mail.gvsu.edu>
2016-09-28 15:05:51 -0400

    Model is functional and Piece has been modified.

--- src/Piece.java
+++ src/Piece.java
@@ -1,8 +1,31 @@
-	/*
-	 * An enum for a player piece
+	/**
+	 * An enumeration for a player piece.
 	 */
 public enum  Piece {
+	/**
+	 * Signifies an empty space on the board.
+	 */
 	EMPTY, 
+	/**
+	 * Signifies an white piece on the board.
+	 */
 	WHITE,
-	BLACK
+	/**
+	 * Signifies a black piece on the board.
+	 */
+	BLACK;
+	
+	/**
+	 * Flips a piece from black to white or vice versa.
+	 * @return This returns the opposite color or empty if it was empty
+	 */
+	public Piece flipPiece() {
+		if (this == WHITE) {
+			return BLACK;
+		}
+		if (this == BLACK) {
+			return WHITE;
+	}
+		return EMPTY;
+	}
 }
