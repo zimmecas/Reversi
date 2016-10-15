270d807030fa59960fe5692493476240514c348d
Casey M. Zimmerman <zimmecas@mail.gvsu.edu>
2016-10-15 11:18:55 -0400

    Added a test class and changed minor project specific setting

---
+++ src/edu/gvsu/scis/cis350/TestModel.java
@@ -0,0 +1,19 @@
+package edu.gvsu.scis.cis350;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestModel {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
